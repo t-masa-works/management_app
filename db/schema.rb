d94d5cf997de91e7a5fab3d2c0561e7d765ceffc
tobe masa <t.masa.works@gmail.com>
2023-08-14 21:57:08 +0900

    TasksのカラムNotNULL制約付与

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_08_14_095803) do
+ActiveRecord::Schema.define(version: 2023_08_14_124857) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
-    t.string "title"
-    t.text "content"
+    t.string "title", null: false
+    t.text "content", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
